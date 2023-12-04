5d83cbbb07462998984a81e93b80d3beeab8451c
Mahasin91 <mahasinmoawad@gmail.com>
2023-12-04 07:50:22 -0500

    commit

--- _printf.c
+++ _printf.c
@@ -19,19 +19,26 @@ format++;
 switch (*format)
 {
 case 'c':
+{
 write_char(va_arg(args, int), &count);
 break;
+}
 case 's':
+{
 write_str(va_arg(args, const char *), &count);
 break;
+}
 case 'd':
 case 'i':
 {
 write_int(va_arg(args, int), &count);
+break;
 }
 case '%':
+{
 write_char('%', &count);
 break;
+}
 default:
 write_char('%', &count);
 write_str(format, &count);
