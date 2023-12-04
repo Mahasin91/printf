719476883d22f186ad61c81948301fb9aa0eabea
Mahasin91 <mahasinmoawad@gmail.com>
2023-12-04 03:38:50 -0500

    commit

--- _printf.c
+++ _printf.c
@@ -26,7 +26,6 @@ int _printf(const char *format, ...)
 				write_str(va_arg(args, const char *), &count);
 				break;
 			case 'd':
-            case 'i':
                 write_int(va_arg(args, int), &count);
                 break;
 			case '%':
