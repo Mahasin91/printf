97eece8441791078a727cfdea0df0072c28257b3
Elyageen <yagosalih452@gmail.com>
2023-12-04 23:45:34 +0300

    fix some bug

--- helper.c
+++ helper.c
@@ -1,9 +1,11 @@
+#include <stdarg.h>
+#include <unistd.h>
 #include "main.h"
 /**
-* write_char - Helper function to write a character to stdout
-* @c: Character to write
-* @count: Pointer to the count of characters
-*/
+ * write_char - Helper function to write a character to stdout
+ * @c: Character to write
+ * @count: Pointer to the count of characters
+ */
 void write_char(char c, int *count)
 {
 	write(1, &c, 1);
@@ -11,10 +13,10 @@ void write_char(char c, int *count)
 }
 
 /**
-* write_str - Helper function to write a string to stdout
-* @str: String to write
-* @count: Pointer to the count of characters
-*/
+ * write_str - Helper function to write a string to stdout
+ * @str: String to write
+ * @count: Pointer to the count of characters
+ */
 void write_str(const char *str, int *count)
 {
 	while (*str)
@@ -26,29 +28,30 @@ void write_str(const char *str, int *count)
 }
 
 /**
-* write_int - Helper function to write an integer
-* @num: integer to write
-* @count: Pointer to the count of numbers
-*/
+ * write_int - Helper function to write an integer
+ * @num: integer to write
+ * @count: Pointer to the count of numbers
+ */
 void write_int(int num, int *count)
 {
-char buffer[12];
-int len = 0;
-if (num < 0)
-{
-write(1, "-", 1);
-(*count)++;
-num = -num;
-}
+	char buffer[12];
 
-do {
-buffer[len++] = num % 10 + '0';
-num /= 10;
-} while (num > 0);
+	int len = 0;
 
-while (len > 0)
-{
-write(1, &buffer[--len], 1);
-(*count)++;
-}
+	if (num < 0)
+	{
+		write(1, "-", 1);
+		(*count)++;
+		num = -num;
+	}
+	do	{
+		buffer[len++] = num % 10 + '0';
+		num /= 10;
+	} while (num > 0);
+
+	while (len > 0)
+	{
+		write(1, &buffer[--len], 1);
+		(*count)++;
+	}
 }
